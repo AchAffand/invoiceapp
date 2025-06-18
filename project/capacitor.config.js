1fb80f87717e5026da1968e8a7c2c2b283be53b7
AchAffand <142283612+AchAffand@users.noreply.github.com>
2025-06-18 15:39:03 +0700

    Add files via upload

---
+++ project/capacitor.config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    appId: 'com.mbakiim.invoiceassistant',
+    appName: 'Invoice Assistant',
+    webDir: 'dist'
+};
+exports.default = config;
---
+++ project/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mbakiim.invoiceassistant',
+  appName: 'Invoice Assistant',
+  webDir: 'dist'
+};
+
+export default config;
